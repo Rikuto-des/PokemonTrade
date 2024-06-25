58429fe4c18f4ee76dcb5f11a456425845004548
RikutoYasuda <ricton513@gmail.com>
2024-06-26 01:01:03 +0900

    Initial Commit

---
+++ PokemonTrade/PokemonTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTradeApp.swift
+//  PokemonTrade
+//
+//  Created by user on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
